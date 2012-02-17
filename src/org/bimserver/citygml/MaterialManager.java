6cae35563bd41a9843f31e1a0d10e00cf0289b3f
Joran Jessurun <A.J.Jessurun@tue.nl>
2012-02-17 15:22:02 +0100

    Implemented a more complex material manager that can assign material based on the Ifc class. Only IfcPlate is added to this yet.
    The setGeometry has been DRY'ed up in a way that is good enough (without introspection).

---
+++ src/org/bimserver/citygml/MaterialManager.java
@@ -0,0 +1,53 @@
+package org.bimserver.citygml;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.bimserver.models.ifc2x3.IfcObjectDefinition;
+import org.bimserver.models.ifc2x3.IfcPlate;
+import org.citygml4j.factory.CityGMLFactory;
+import org.citygml4j.model.citygml.appearance.Appearance;
+import org.citygml4j.model.citygml.appearance.Color;
+import org.citygml4j.model.citygml.appearance.X3DMaterial;
+import org.citygml4j.model.citygml.core.CityModel;
+import org.citygml4j.model.gml.base.AbstractGML;
+import org.citygml4j.model.gml.geometry.primitives.AbstractSurface;
+
+class MaterialManager {
+	private CityGMLFactory citygml;
+	private Map<Class<?>,X3DMaterial> material = new HashMap<Class<?>,X3DMaterial>();
+	private Appearance appearance;
+	
+	MaterialManager(CityModel cityModel, CityGMLFactory citygml) {
+		this.citygml = citygml;
+		appearance = citygml.createAppearance();
+		appearance.setTheme("ifc2citygml");
+		cityModel.addAppearanceMember(citygml.createAppearanceMember(appearance));
+	
+		createMaterial(IfcPlate.class, citygml.createColor(0.7, 0.7, 1.0), 0.9);
+	}
+
+	private void createMaterial(Class<?> klass, Color diffuseColor, double transparency) {
+		X3DMaterial material = citygml.createX3DMaterial();
+		material.setDiffuseColor(diffuseColor);
+		material.setTransparency(transparency);
+		appearance.addSurfaceDataMember(citygml.createSurfaceDataProperty(material));
+		this.material.put(klass, material);
+	}
+
+	public void assign(AbstractSurface compositeSurface, IfcObjectDefinition ifcObjectDef) {
+		for(Map.Entry<Class<?>,X3DMaterial> entry : material.entrySet()) {
+			if(entry.getKey().isAssignableFrom(ifcObjectDef.getClass())) {
+				entry.getValue().addTarget(hrefTo(compositeSurface));
+			}
+		}				
+	}		
+	
+	private String hrefTo(AbstractGML abstractGML) {
+		if(abstractGML.getId() == null) {
+			abstractGML.setId(UUID.randomUUID().toString());
+		}
+		return "#" + abstractGML.getId();
+	}	
+}
