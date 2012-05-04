8fdbbd6121b5e7e482b12af70aa7feeb578a0ed4
joran.jessurun <joran.jessurun@localhost>
2012-05-04 17:07:38 +0200

    Generalized the exporting of products that are installations in CityGML

---
+++ src/org/bimserver/citygml/Product2InstallationInfo.java
@@ -0,0 +1,49 @@
+package org.bimserver.citygml;
+
+public class Product2InstallationInfo {
+	private String pSet;
+	private String internalFunction;
+	private String externalFunction;
+	private boolean defaultExternal;
+
+	public Product2InstallationInfo(String pSet, String internalFunction,
+			String externalFunction, boolean defaultExternal) {
+		super();
+		this.pSet = pSet;
+		this.internalFunction = internalFunction;
+		this.externalFunction = externalFunction;
+		this.defaultExternal = defaultExternal;
+	}
+
+	public String getpSet() {
+		return pSet;
+	}
+
+	public void setpSet(String pSet) {
+		this.pSet = pSet;
+	}
+
+	public String getInternalFunction() {
+		return internalFunction;
+	}
+
+	public void setInternalFunction(String internalFunction) {
+		this.internalFunction = internalFunction;
+	}
+
+	public String getExternalFunction() {
+		return externalFunction;
+	}
+
+	public void setExternalFunction(String externalFunction) {
+		this.externalFunction = externalFunction;
+	}	
+	
+	public boolean isDefaultExternal() {
+		return defaultExternal;
+	}
+
+	public void setDefaultExternal(boolean defaultExternal) {
+		this.defaultExternal = defaultExternal;
+	}
+}
